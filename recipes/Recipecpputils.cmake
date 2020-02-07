8ee0458659e80d5ef420ab6d3fc3d49a099d6211
julibert <julianbermudez@eprosima.com>
2020-02-07 18:46:38 +0100

    Add julibert::cpputils recipe.

---
+++ recipes/Recipecpputils.cmake
@@ -0,0 +1,30 @@
+# Copyright 2020-present Julián Bermúdez Ortega.
+#
+# This file is part of julibert::harvester.
+#
+# julibert::harvester is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# julibert::harvester is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with julibert::harvester.  If not, see <https://www.gnu.org/licenses/>.
+
+include(ExternalProject)
+
+ExternalProject_Add(cpputils
+    GIT_REPOSITORY
+        git@github.com:julibert/cpputils.git
+    GIT_TAG
+        master
+    INSTALL_DIR
+        ${RECIPE_INSTALL_PATH}
+    CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+        -DCPPUTILS_BUILD_TESTS:BOOL=OFF
+    )
